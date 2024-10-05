3bc34da9242e4623c0b40f4101c5a1c66ab0726f
Yaroslav <qustoo@yandex.ru>
2024-10-05 23:58:44 +0300

    consts values

---
+++ app/consts/consts_values.py
@@ -0,0 +1,3 @@
+RPS_LIMIT = 1000  # Лимит запросов в секунду
+WINDOW_SIZE = 1  # Размер окна в секундах
+LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s" # Формат логов
