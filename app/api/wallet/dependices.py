1bcdbc8dda3c07a54e42ed4133726107f1d68bf2
Yaroslav <qustoo@yandex.ru>
2024-10-14 15:11:24 +0300

    Fetch the wallet with a FOR UPDATE lock

--- app/api/wallet/dependices.py
+++ app/api/wallet/dependices.py
@@ -1,16 +1,25 @@
 from typing import Annotated
 from uuid import UUID
 
+from fastapi import Depends, HTTPException, Path, status
+from sqlalchemy import select
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from core.database.helper import database_helper
 from core.models.business_models import Wallet
-from fastapi import Depends, HTTPException, Path, status
-from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def get_wallet_object(
-    session: Annotated[AsyncSession, Depends(database_helper.session_getter)], wallet_id: UUID = Path(...)
+        session: Annotated[AsyncSession, Depends(database_helper.session_getter)], wallet_id: UUID = Path(...)
 ):
-    wallet = await session.get(Wallet, wallet_id)
-    if not wallet:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Wallet not found")
-    return wallet
+    try:
+        stmt = select(Wallet).where(Wallet.id == wallet_id).with_for_update()
+        scalars_result = await session.scalars(stmt)
+        wallet = scalars_result.first()
+        if not wallet:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Wallet not found")
+        return wallet
+    except OperationalError as exc:
+        await session.rollback()
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Exception: {str(exc)}")
